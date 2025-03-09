33d996cef45d1c62cf7841927fb6dd8f1e676739
gabriel-lunesu <g.lunesu@outlook.com>
2025-03-09 19:39:33 +0100

    rounded

--- src/app/components/new/Hero.js
+++ src/app/components/new/Hero.js
@@ -81,7 +81,7 @@ export default function Hero() {
                 </div>
               </div>
               <div className="absolute bottom-0 left-0 right-0 text-center -mb-12">
-                <h2 className="text-2xl md:text-3xl font-bold text-white bg-blue-500 inline-block px-4 py-2">
+                <h2 className="text-2xl md:text-3xl font-bold rounded-full text-white bg-blue-500 inline-block px-4 py-2">
                   Donato <span className="text-blue-900">Lunesu</span>
                 </h2>
               </div>
